4db6d9e8fdf5a7b5e0ed09cbf04968a7d1b0758b
camiula <camilacarwalho3@gmail.com>
2018-08-24 13:13:59 -0300

    Camila - JavaDOC em Cozinha

--- src/com/ifpb/projeto/model/Cozinha.java
+++ src/com/ifpb/projeto/model/Cozinha.java
@@ -3,15 +3,30 @@ package com.ifpb.projeto.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe Cozinha modela a entidade cozinha do domínio da aplicação.
+ *   Composto por uma lista de pedidos
+ *   Adicionar e atender pedidos
+ *   Essa classe foi simplificada para <b>facilitar o desenvolvimento</b>
+ *   @author Camila Carvalho
+ *   @author Mailson Dennis
+ *   @since 26-07-2018
+ *   @version 1.0
+ */
+
+
 public class Cozinha {
     private List<Pedido> pedidos;
 
-    //Construtor
+
+    /**
+     * Construtor da classe.
+     */
     public Cozinha() {
         pedidos = new ArrayList<Pedido>();
     }
 
-    //Getters and Setters
+
 
     public List<Pedido> getPedidos() {
         return pedidos;
@@ -21,6 +36,14 @@ public class Cozinha {
         this.pedidos = pedidos;
     }
 
+
+    /**
+     * Retornar um pedido pelo seu id
+     * @param idPedido : número do id do pedido desejado
+     * @return o pedido encontrado
+     * @return nulo caso o pedido não seja encontrado
+     */
+
     public Pedido getPedido(int idPedido){
         for (Pedido pedido:pedidos) {
             if(pedido.getNumeroPedido()==idPedido){
@@ -29,12 +52,21 @@ public class Cozinha {
         }
         return null;
     }
-
+    /**
+     * Adicionar um objeto do tipo Pedido
+     * @param pedido : objeto
+     */
     public void adicionarPedido(Pedido pedido){
         pedidos.add(pedido);
 
     }
-
+    /**
+     * Remover um pedido da Lista da cozinha
+     * Atende o pedido
+     * @param idPedido : número do id do pedido desejado
+     * @return true caso o pedido seja removido
+     * @return false caso o pedido já tenha sido atendido
+     */
     //Esta função remove um pedido da Lista da cozinha;
     public boolean atendePedido(int idPedido){
         if(getPedido(idPedido)==null) return false;
