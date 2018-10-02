c0f44abe1fd6f34e9a4eb41409e3bb649593e500
camiula <camilacarwalho3@gmail.com>
2018-10-02 10:34:00 -0300

    Mais telas, falta so deixar bonitinho

---
+++ src/com/ifpb/projeto/view/Cozinha.java
@@ -0,0 +1,9 @@
+package com.ifpb.projeto.view;
+
+import javax.swing.*;
+
+public class Cozinha {
+    private JPanel panel1;
+    private JButton atenderButton;
+    private JComboBox comboBox1;
+}
--- src/com/ifpb/projeto/view/EditarPedido.java
+++ src/com/ifpb/projeto/view/EditarPedido.java
@@ -1,4 +1,10 @@
 package com.ifpb.projeto.view;
 
+import javax.swing.*;
+
 public class EditarPedido {
+    private JButton excluirButton;
+    private JButton modificarButton;
+    private JTextField textField2;
+    private JComboBox comboBox1;
 }
