5dc014b6046912649e39f6eb80399e8b8bee0a13
camiula <camilacarwalho3@gmail.com>
2018-10-05 21:42:01 -0300

    Tela de minha conta

---
+++ src/com/ifpb/projeto/view/MinhaConta.java
@@ -0,0 +1,23 @@
+package com.ifpb.projeto.view;
+
+import javax.swing.*;
+
+public class MinhaConta extends JDialog {
+    private JPanel panel1;
+    private JButton alterarDadosButton;
+    private JButton editarSenhaButton;
+    private JButton excluirContaButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+
+    public MinhaConta(){
+        setContentPane(panel1);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+
+
+}
